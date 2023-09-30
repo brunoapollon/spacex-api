c0d2b989cbac207e83983f24912d94e8dde57425
Bruno Lopes <eusoubruno7@gmail.com>
2023-09-30 20:28:49 -0300

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 require('dotenv/config');
 const express = require('express');
+const cors = require('./app/middlewares/cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 
 app.listen(3001, () => console.log('🚀 server started at http://localhost:3000'));
