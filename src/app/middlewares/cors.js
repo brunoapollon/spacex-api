94b739ee90adc2f6316ba7342c6dde731eaebe5a
Bruno Lopes <eusoubruno7@gmail.com>
2023-10-03 21:44:23 -0300

    fix: adjusts

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = function cors(request, response, next) {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Origin', process.env.FRONT_END_URL);
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
   response.setHeader('Access-Control-Max-Age', '10');
