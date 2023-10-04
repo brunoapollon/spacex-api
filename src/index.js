a38b82e67316ee7c51642843e70384d7b487b952
Bruno Lopes <eusoubruno7@gmail.com>
2023-10-04 07:17:10 -0300

    fix: fix port and update env example

--- src/index.js
+++ src/index.js
@@ -13,4 +13,6 @@ app.use(errorHandler);
 app.use(cors);
 app.use(router);
 
-app.listen(3001, () => console.log('🚀 server started at http://localhost:3001'));
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log('🚀 server started'));
